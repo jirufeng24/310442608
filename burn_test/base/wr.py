d69296b65cdc58b981e47495d4ac35eec660f141
jirufeng <310442608@qq.com>
2022-02-19 13:23:51 +0800

    重新提交嗲吗

--- burn_test/base/wr.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path1 = os.path.realpath(__file__)
-print("path1=",path1)
-rootpath = os.path.dirname(os.path.split(path1))
-print(path1, rootpath)
-
-chrome_driver_path = rootpath + "\chromedriver.exe"
